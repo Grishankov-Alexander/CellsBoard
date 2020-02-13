841c3590c9debce0b6040b7d9ed370f621689e37
ache2014 <ache2014@gmail.com>
2020-02-13 17:08:44 +0300

    Вводит некоторые логические изменения в работе.

--- cells_interaction.py
+++ cells_interaction.py
@@ -2,16 +2,19 @@ import random
 import time
 import sys
 import copy
+import argparse
+import re
 
 
 class Cell(object):
+    """Represents dead(0) or alive(1) cell object"""
+
     ALIVE = 1
     DEAD = 0
     def __init__(self, state):
         if state not in [self.DEAD, self.ALIVE]:
             raise ValueError("Cell could be either DEAD(0) or ALIVE(1)")
         self.state = state
-        self.neighbors = []
 
     def __repr__(self):
         return f"Cell({self.state})"
@@ -21,27 +24,28 @@ class Cell(object):
         
         
 class CellsBoard(object):
-    def __init__(self, rows=10, columns=10, fromFile=False):
-        self.rows = rows
-        self.columns = columns
-        self.fromFile = fromFile
-        if self.fromFile is False:
+    """2-dimensional board of Cell objects."""
+
+    def __init__(self, rows, columns, file=None):
+        """Create rows x columns board.
+
+        Create from file if specified.
+        else - fill the board with random Cell objects"""
+        if file:
+            self.initFromFile(file)
+        else:
+            self.rows = rows
+            self.columns = columns
             # Create a 2D board with random state cells
             random.seed()
             self.board = [
                 [Cell(random.randint(Cell.DEAD, Cell.ALIVE))
                  for j in range(self.columns)] for i in range(self.rows)
                 ]
-            # Find neighbors for each cell.
-            for row in range(self.rows):
-                for column in range(self.columns):
-                    cell = self.board[row][column]
-                    cell.neighbors = self.findNeighbors(row, column)
-#TODO: initialize from file
     
     def __repr__(self):
         """Can be used to recreate a CellsBoard"""
-        return f"CellsBoard({self.rows}, {self.columns}, {self.fromFile})"
+        return f"CellsBoard({self.rows}, {self.columns}, {self.file})"
 
     def __str__(self):
         """Returns printable view of a CellsBoard"""
@@ -54,15 +58,30 @@ class CellsBoard(object):
             boardView += " ".join(strBoard[i]) + "\n"
         return boardView
 
-    def findNeighbors(self, row: int, column: int) -> list:
+    def initFromFile(self, file):
+        """Initialize the board from file."""
+        with open(file) as fp:
+            # Get rid of empty lines and spaces
+            lines = [
+                re.sub(r"\s*", "", line) for line in fp.readlines()
+                if re.sub(r"\s*", "", line)
+                ]
+            self.rows = len(lines)
+            self.columns = len(lines[0])
+            self.board = [
+                [Cell(int(lines[i][j]))
+                 for j in range(self.columns)] for i in range(self.rows)
+                ]
+            
+    def findNeighbors(self, board, row: int, column: int) -> list:
         """Returns a list of neighbors for a board[row][column] cell."""
         neighbors = [
-            self.board[i][j]
+            board[i][j]
             for i in range(row - 1, row + 2)
-            if i >= 0 and i < len(self.board)
+            if i >= 0 and i < len(board)
             for j in range(column - 1, column + 2)
-            if j >= 0 and j < len(self.board[i])
-            and self.board[i][j] != self.board[row][column]
+            if j >= 0 and j < len(board[i])
+            and board[i][j] != board[row][column]
             ]
         return neighbors
 
@@ -72,8 +91,9 @@ class CellsBoard(object):
         for row in range(self.rows):
             for column in range(self.columns):
                 cell = boardCopy[row][column]
+                cellNeighbors = self.findNeighbors(boardCopy, row, column)
                 aliveNeighbors = len(
-                    [neighbor for neighbor in cell.neighbors
+                    [neighbor for neighbor in cellNeighbors
                      if neighbor.state == Cell.ALIVE]
                     )
                 if cell.state == Cell.ALIVE:
@@ -83,10 +103,22 @@ class CellsBoard(object):
                     if aliveNeighbors == 3:
                         self.board[row][column].state = Cell.ALIVE
 
-    
+
 if __name__ == "__main__":
-    board = CellsBoard()
+    parser = argparse.ArgumentParser(
+        description="Simulate cells interaction in the 2D board"
+        )
+    parser.add_argument("rows", type=int, nargs="?", default=10,
+                        help="Number of rows in the 2D board.")
+    parser.add_argument("columns", type=int,nargs="?", default=10,
+                        help="Number of columns in the row")
+    parser.add_argument("-f", "--file", help="Init a board from file.")
+    args = parser.parse_args()
+    if args.file:
+        board = CellsBoard(args.rows, args.columns, file = args.file)
+    else:
+        board = CellsBoard(args.rows, args.columns)
     while True:
         print(board)
         board.updateBoard()
-        #time.sleep(1)
+        time.sleep(1)
